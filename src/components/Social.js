4e8aa57b816c4b74dee905556bf541c83f07dd51
Jinwoo-1162 <jinwoo1162@gmail.com>
2021-12-22 00:58:22 -0800

    fixed outdated github url

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,7 +4,7 @@ import '../CSS/Social.css';
 export const Social = (props) => {
     return (
         <div className="social">
-            <a href="https://github.gatech.edu/jpark955" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://github.com/Jinwoo-1162" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
             <a href="https://www.linkedin.com/in/jinwoo-park-gt/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
         </div>
     );
